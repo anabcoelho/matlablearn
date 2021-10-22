fb505fe6f8d30703e46b4f3cf4df4b029e0b88b9
anabcoelho <anabcoelh@gmail.com>
2021-10-22 19:30:24 -0300

    del

--- Dados/BreatheEasyEDA-master/BEEDA/gui_files/downsample_BEEDAdata.m
+++
@@ -1,9 +0,0 @@
-function [ BEEDAdata ] = downsample_BEEDAdata(BEEDAdata,factor)
-
-BEEDAdata.respiration = BEEDAdata.respiration(factor:factor:end);
-BEEDAdata.EDAdata = BEEDAdata.EDAdata(factor:factor:end);
-BEEDAdata.sampletimes = BEEDAdata.sampletimes(factor:factor:end);
-BEEDAdata.downsample_factor = factor;
-
-end
-
